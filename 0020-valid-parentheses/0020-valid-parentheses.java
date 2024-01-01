f4653ed0f30d553dd7a607684879a9ef0f54d761
Leo D. Penrose <90817505+leodvincci@users.noreply.github.com>
2024-01-01 15:54:28 -0600

    Time: 4 ms (6.74%), Space: 41.7 MB (5.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map myMap = new HashMap<Character, Character>();
+        Stack myStack = new Stack<Character>();
+        myMap.put('(', ')');
+        myMap.put('{', '}');
+        myMap.put('[', ']');
+    
+
+        for(Character c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                myStack.push(c);
+            }else{
+                if(myStack.isEmpty()){
+                    return false;
+                }else {
+                    Object popChar = myStack.pop();
+                    boolean compare = myMap.get(popChar).equals(c);
+                    if(compare == false){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return myStack.isEmpty();
+    }
+}
