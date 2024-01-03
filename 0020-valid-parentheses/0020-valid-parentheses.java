ec345ab15c8cc7f629612297339a9c83d51ae57a
Leo D. Penrose <90817505+leodvincci@users.noreply.github.com>
2024-01-02 21:05:40 -0600

    Time: 4 ms (7.02%), Space: 41.3 MB (17.8%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Map myMap = new HashMap<Character, Character>();
-        Stack myStack = new Stack<Character>();
-        myMap.put('(', ')');
-        myMap.put('{', '}');
-        myMap.put('[', ']');
-    
+        
+        HashMap<Character,Character> myHash = new HashMap<>();
+        Stack<Character> myStack = new Stack<>();
+        myHash.put('(', ')');
+        myHash.put('{', '}');
+        myHash.put('[' , ']');
 
         for(Character c : s.toCharArray()){
-            if(c == '(' || c == '{' || c == '['){
+            if( myHash.containsKey(c) ){
                 myStack.push(c);
             }else{
-                if(myStack.isEmpty()){
+                if (myStack.isEmpty()){
                     return false;
-                }else {
-                    Object popChar = myStack.pop();
-                    boolean compare = myMap.get(popChar).equals(c);
-                    if(compare == false){
+                }else{
+                    Character comp = myStack.pop();
+                    if(c != myHash.get(comp) ){
                         return false;
                     }
                 }
             }
         }
+        if(myStack.isEmpty()){
+            return true;
+        }
 
-        return myStack.isEmpty();
+        return false;
+
+
     }
 }
